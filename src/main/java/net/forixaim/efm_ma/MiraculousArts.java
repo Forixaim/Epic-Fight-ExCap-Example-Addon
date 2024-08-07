bf210d17ad803136bd801a878d38714f46fec215
Matthew <thebest20067@gmail.com>
2024-08-07 10:49:25 -0700

    Stripped the existing battle arts and turned it into an example addon.

---
+++ src/main/java/net/forixaim/efm_ma/MiraculousArts.java
@@ -0,0 +1,31 @@
+package net.forixaim.efm_ma;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MiraculousArts.MOD_ID)
+public class MiraculousArts
+{
+
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "efm_ma";
+
+    public MiraculousArts() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+}
